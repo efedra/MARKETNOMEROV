e4d96255ce9e2eb403e1d3f75d2c36c0dde34707
efedra <efedra-00@mail.ru>
2023-11-19 21:44:00 +0300

    add arrow

--- js/list.js
+++ js/list.js
@@ -4,7 +4,7 @@ lists.forEach(el => {
     el.addEventListener('click', (e) => {
         if (e.target.closest('.sort-filter__input')) {
             el.querySelector('.sort-filter__input').classList.toggle('sort-filter__input_active')
-            if(el.querySelector('.sort-filter__list')){
+            if (el.querySelector('.sort-filter__list')) {
                 el.querySelector('.sort-filter__list').classList.toggle('sort-filter__list_hidden')
             }
         }
@@ -38,25 +38,55 @@ document.addEventListener('click', (event) => {
     const selectFilterList = document.querySelector('.select-options__list');
     const selectFilterInput = document.querySelector('.goods-input__search');
 
-    if (!selectFilterElem.contains(event.target)) {
+    if (!selectFilterElem.contains(event.target) && !selectFilterInput.contains(event.target)) {
         selectFilterElem.classList.remove('select-options__elem_active')
-            selectFilterList.classList.add('sort-filter__list_hidden');
-            selectFilterInput.classList.add('sort-filter__list_hidden');
+        selectFilterList.classList.add('sort-filter__list_hidden');
+        selectFilterInput.classList.add('sort-filter__list_hidden');
+    }
+
+    let inputCodeFullList = document.querySelector('.input-code-full-list');
+    let inputCodeList = document.querySelector('.input-code__list');
+
+    if (!inputCodeFullList.contains(event.target)) {
+        inputCodeList.classList.add('sort-filter__list_hidden'); // Закрытие списка кода страны при клике вне него
     }
 });
 
 //ВЫБЕРИТЕ РЕГИОН
-
 let listsSelectOptions = document.querySelectorAll('.select-options__full-list')
 
-listsSelectOptions.forEach(list=>{
-    list.addEventListener('click',(e)=>{
+listsSelectOptions.forEach(list => {
+    list.addEventListener('click', (e) => {
         if (e.target.closest('.select-options__elem')) {
             list.querySelector('.select-options__elem').classList.toggle('select-options__elem_active')
-            if( list.querySelector('.select-options__list') && list.querySelector('.goods-input__search')){
+            if (list.querySelector('.select-options__list') && list.querySelector('.goods-input__search')) {
                 list.querySelector('.goods-input__search').classList.toggle('sort-filter__list_hidden')
                 list.querySelector('.select-options__list').classList.toggle('sort-filter__list_hidden')
             }
         }
-    })
-})
+    });
+});
+
+//КОД СТРАНЫ
+let inputCodeFullList = document.querySelector('.input-code-full-list');
+let inputCodeNumber = document.querySelector('.input-code__number');
+let inputCodeList = document.querySelector('.input-code__list');
+let codeListElems = document.querySelectorAll('.sort-filter__list-elem');
+
+inputCodeFullList.addEventListener('click', (event) => {
+    inputCodeList.classList.toggle('sort-filter__list_hidden');
+});
+
+codeListElems.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+        codeListElems.forEach((el) => {
+            el.classList.remove('sort-filter__list-elem_active');
+        });
+        elem.classList.add('sort-filter__list-elem_active');
+        inputCodeNumber.textContent = elem.textContent;
+    });
+});
+
+
+
+
--- js/tabs.js
+++ js/tabs.js
@@ -2,16 +2,17 @@ let tabsGoods = document.querySelectorAll('.goods_nav div')
 let allGoodsSliders = document.querySelectorAll('.goods__slider')
 
 
+allGoodsSliders.forEach(el => {
+    if (el.getAttribute('data-goods') !== '1') {
+        el.style.visibility = 'hidden'
+    }
+})
 
-    allGoodsSliders.forEach(el => {
-        if (el.getAttribute('data-goods') !== '1') {
-            el.style.visibility = 'hidden'
-        }
-    })
-
-    tabsGoods.forEach(el => {
-        el.addEventListener('click', () => {
+tabsGoods.forEach(el => {
+    el.addEventListener('click', () => {
+        if (document.querySelector(`.goods__slider[data-goods="${el.getAttribute('data-goods')}"]`)) {
             let neededSlider = document.querySelector(`.goods__slider[data-goods="${el.getAttribute('data-goods')}"]`);
+            console.log(neededSlider)
             neededSlider.style.visibility = 'visible'
             allGoodsSliders.forEach(e => {
                     if (e.getAttribute('data-goods') !== neededSlider.getAttribute('data-goods')) {
@@ -19,31 +20,30 @@ let allGoodsSliders = document.querySelectorAll('.goods__slider')
                     }
                 }
             )
-        })
+        }
     })
+})
 
 
-
-let tabsTarif = document.querySelectorAll('.slider-tarif-container div')
+let tabsTarif = document.querySelectorAll('.tarif-nav div')
 let allTarifSliders = document.querySelectorAll('.slider-tarif')
 
 
+allTarifSliders.forEach(el => {
+    if (el.getAttribute('data-tarif') !== '1') {
+        el.style.visibility = 'hidden'
+    }
+})
 
-    allTarifSliders.forEach(el => {
-        if (el.getAttribute('data-tarif') !== '1') {
-            el.style.visibility = 'hidden'
-        }
-    })
-
-    tabsTarif.forEach(el => {
-        el.addEventListener('click', () => {
-            let neededSlider = document.querySelector(`.goods__slider[data-goods="${el.getAttribute('data-tarif')}"]`);
-            neededSlider.style.visibility = 'visible'
-            allTarifSliders.forEach(e => {
-                    if (e.getAttribute('data-tarif') !== neededSlider.getAttribute('data-tarif')) {
-                        e.style.visibility = 'hidden'
-                    }
+tabsTarif.forEach(el => {
+    el.addEventListener('click', () => {
+        let neededSlider = document.querySelector(`.slider-tarif[data-tarif="${el.getAttribute('data-tarif')}"]`);
+        neededSlider.style.visibility = 'visible'
+        allTarifSliders.forEach(e => {
+                if (e.getAttribute('data-tarif') !== neededSlider.getAttribute('data-tarif')) {
+                    e.style.visibility = 'hidden'
                 }
-            )
-        })
+            }
+        )
     })
+})
