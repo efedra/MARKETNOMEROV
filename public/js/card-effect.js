072483703377bf8ce6b02a29799b9954d26172ea
efedra <efedra-00@mail.ru>
2023-11-21 21:59:24 +0300

    card effect done

--- public/js/card-effect.js
+++ public/js/card-effect.js
@@ -4,3 +4,15 @@ let swiper = new Swiper('.Slider-container',{
     centerdSlides: true,
     loop: true,
 })
+
+const leftArrow = document.querySelector('.arrows__left_orange');
+const rightArrow = document.querySelector('.arrows__right_orange');
+
+// Добавляем обработчики событий для стрелок
+leftArrow.addEventListener('click', () => {
+    swiper.slidePrev(); // Перелистываем на предыдущий слайд
+});
+
+rightArrow.addEventListener('click', () => {
+    swiper.slideNext(); // Перелистываем на следующий слайд
+});
